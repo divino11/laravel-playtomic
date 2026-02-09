ef91f873bf4b9a5e06f5a5e0fe402454ad6b07af
Andrey Vasyukov <a.vasyukov97@gmail.com>
2026-02-09 23:51:47 +0800

    chore: remove vendor/, composer.lock, duplicate src/src/, add .gitignore
    - Add .gitignore to exclude vendor/, composer.lock, .phpunit.cache
    - Remove vendor/ directory from git (8,600+ files)
    - Remove duplicate src/src/ directory
    - Add cancelBooking() method to auth client interface and implementation
    - Update README with booking and cancel booking documentation

--- src/src/Exceptions/PlaytomicApiException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Divino11\Playtomic\Exceptions;
-
-class PlaytomicApiException extends PlaytomicException {}
--- src/src/Exceptions/PlaytomicException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Divino11\Playtomic\Exceptions;
-
-use RuntimeException;
-
-abstract class PlaytomicException extends RuntimeException {}
